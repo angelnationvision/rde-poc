dff11a63f7638ac70745aeaea022e2ff84177aac
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-06-23 12:25:14 -0700

    adding column

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -60,4 +60,23 @@ export default async function decorate(block) {
     details.append(summary, body);
     panel.append(details);
   });
+};
+
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach(row => {
+    [...row.children].forEach(col => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
 }
