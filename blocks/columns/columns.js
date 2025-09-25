74a6402696f6479afb25fb54b9c48918902785a8
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-25 09:20:16 -0700

    updates

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add('columns-main');
+  console.log("columns");
 
   // setup columns
   [...block.children].forEach((row) => {
