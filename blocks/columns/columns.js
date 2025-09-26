0ceb8a9ebb5bb4d8f3355794c96c3b0531a4cbe4
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 15:35:47 -0700

    goback

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -25,9 +25,9 @@ const gapClass = gapName[1];
     const matching_elements = all_elements_array.filter(element => element.innerHTML.includes(colnode));
     console.log('Found elements:', matching_elements);
     const getElement = matching_elements.at(-1);
-    const getClassname = getElement[0].innerHTML();
+    //const getClassname = getElement.innerHTML();
     console.log("get Element", getElement);
-    console.log("get classname", getClassname);
+    //console.log("get classname", getClassname);
 
 
 //Good Code Below
