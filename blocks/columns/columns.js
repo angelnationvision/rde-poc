467c6df764a4e9cc62546058ad8bd716e140e480
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 16:43:37 -0700

    movecode

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -4,6 +4,16 @@ const attr = document.querySelector('[class*="gap-"]');
 const gapName = attr.className.split(" ");
 const gapClass = gapName[1];
 
+const colnode = "colnode-";
+const all_elements_array = Array.from(document.querySelectorAll('*')); 
+const matching_elements = all_elements_array.filter(element => element.innerHTML.includes(colnode));
+            const getElement = matching_elements.at(-1);
+            console.log(getElement);
+            const eleString = getElement.textContent.trim().replace('colnode-', '');
+            console.log(eleString);
+       
+        col.classList.add(eleString);
+
 // setup columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
@@ -20,9 +30,7 @@ const gapClass = gapName[1];
 
       //Created colnode prefix in case others use the classname within their blocks 
             const colnode = "colnode-";
-           // const all_elements_array = Array.from(document.querySelectorAll('*'));
-            const all_elements_array = Array.from(col);
-            
+            const all_elements_array = Array.from(document.querySelectorAll('*')); 
             const matching_elements = all_elements_array.filter(element => element.innerHTML.includes(colnode));
             const getElement = matching_elements.at(-1);
             console.log(getElement);
