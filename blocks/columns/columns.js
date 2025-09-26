bcd942be60c816eff8df8b7706bccfaec461b25d
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 13:58:46 -0700

    trythis

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -17,24 +17,17 @@ const gapClass = gapName[1];
         }
       }
       const nodeList = col; 
-      const searchText = "colspan-5";
+      const searchText = "colspan-";
       console.log(nodeList.innerHTML.includes(searchText));
 
       if (nodeList.innerHTML.includes(searchText)) {
+        const getcolSpan = nodeList.innerHTML.includes(searchText);
+        console.log(getcolSpan);
         console.log("Yes has object");
+
       }
       else {
         console.log("nope");
-      }
-    
-      const matchingElements = Array.from(nodeList).filter(element => {
-        return element.innerHTML.includes(searchText);
-      });
-
-      if (matchingElements.length > 0) {
-        console.log("Found matching elements:", matchingElements);
-      } else {
-        console.log("No matching elements found.");
       }
 
     });
