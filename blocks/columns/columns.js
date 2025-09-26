d76a207a93b37411b1c810736088ef713ca330b3
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 16:37:09 -0700

    trythis

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -21,6 +21,10 @@ const gapClass = gapName[1];
       //Created colnode prefix in case others use the classname within their blocks 
         const colnode = "colnode-";
         const all_elements_array = Array.from(document.querySelectorAll('*'));
+
+        all_elements_array.forEach(function(number, index) {
+            console.log(`Index: ${index}, Value: ${number}`);
+        });
         const matching_elements = all_elements_array.filter(element => element.innerHTML.includes(colnode));
         const getElement = matching_elements.at(-1);
         console.log(getElement);
