b25d47c03b2ee12efbacb7a8d9c1809c7ecdc561
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 16:05:11 -0700

    trythis

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -16,10 +16,7 @@ const gapClass = gapName[1];
           picWrapper.classList.add('columns-img-col');
         }
       }
-
-  
     const colnode = "colnode-";
-    var nodeList = Array.from(col);
 
      if (col.innerHTML.includes(colnode)) {
         const all_elements_array = Array.from(document.querySelectorAll('*'));
@@ -27,10 +24,10 @@ const gapClass = gapName[1];
         console.log('Found elements:', matching_elements);
         const getElement = matching_elements.at(-1);
         console.log("get Element", getElement);
-        const eleString = String(getElement).textContent;
+        const eleString = getElement.textContent.trim();
         console.log(eleString);
-        const trimElement = eleString.htmlString.replace(/<p>/g, "").replace(/<\/p>/g, "");
-        console.log(trimElement);
+        //const trimElement = eleString.htmlString.replace(/<p>/g, "").replace(/<\/p>/g, "");
+        //console.log(trimElement);
 
         //const remove = getElement.remove();
        }
