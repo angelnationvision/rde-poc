ed542621df41c4fc43e79b59e140941a8285a109
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 16:20:45 -0700

    trythis

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -21,7 +21,7 @@ const gapClass = gapName[1];
       //Created colnode prefix in case others use the classname within their blocks 
      const colnode = "colnode-";
      if (col.innerHTML.includes(colnode)) {
-        col.parentElement.removeAttribute("style",`grid-template-columns: repeat(${cols.length}, 1fr)`);
+        col.parentElement.removeAttribute("style");
         const all_elements_array = Array.from(document.querySelectorAll('*'));
         const matching_elements = all_elements_array.filter(element => element.innerHTML.includes(colnode));
         const getElement = matching_elements.at(-1);
