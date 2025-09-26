5b66d912bb7fcf9de09a0d0126e4a2d8563a1fef
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 15:32:04 -0700

    sdfs

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -24,7 +24,7 @@ const gapClass = gapName[1];
     const all_elements_array = Array.from(document.querySelectorAll('*'));
     const matching_elements = all_elements_array.filter(element => element.innerHTML.includes(colnode));
     console.log('Found elements:', matching_elements);
-    const getElement = matching_element.at(-1);
+    const getElement = matching_elements.at(-1);
     console.log("get Element", getElement)
 
 
