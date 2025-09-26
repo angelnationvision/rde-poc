28a702915d718a28e02af7e78bd0d50cc5911a6c
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-26 11:59:11 -0700

    trythis

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -4,12 +4,10 @@ const cols = [...block.firstElementChild.children];
   // setup columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
-      console.log(col.className);
-      col.classList.remove ('columns');
-      const attr = document.querySelector('[class*="gap-"]');
+      const attr = document.querySelector('[class*="gap-"][2]');
       console.log(attr.className);
 
-      col.parentElement.classList.add('grid-container', attr);
+      col.parentElement.classList.add('grid-container');
       col.classList.add('colspan-default'); 
       const pic = col.querySelector('picture');
       if (pic) {
@@ -22,5 +20,5 @@ const cols = [...block.firstElementChild.children];
     });
   });
 
-  block.classList.add('columns-main columns');
+  block.classList.add('columns-main');
 }
