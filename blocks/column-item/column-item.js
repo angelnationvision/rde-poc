533bd67c5ef9d3df362271f824c713e7dfe58319
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-27 08:50:40 -0700

    tryandsee

--- blocks/column-item/column-item.js
+++ blocks/column-item/column-item.js
@@ -1,8 +1,1 @@
-export default function decorate(block) {
-    console.log("column item");
-const colspan = document.querySelector('[class*="colspan"]');
-    console.log(colspan);
- if (colspan) {
-    console.log("colspan is here");
- }
-}
+
--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -30,6 +30,7 @@ const gapClass = gapName[1];
             }    
            if (col.innerHTML.includes(colgrid)) {
               const matching_elements_cg = all_elements_array.filter(element => element.innerHTML.includes(colgrid));
+              console.log(matching_elements_cg);
               const getElementcg = matching_elements_cg.at(-1);
               const eleStringcg = getElementcg.textContent.trim();
               col.classList.add(eleStringcg);
