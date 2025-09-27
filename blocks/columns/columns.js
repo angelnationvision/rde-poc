d4f9886a83d17f8d24818a24be5c5ad468b7b3cc
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-27 09:54:49 -0700

    tryti

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -11,8 +11,8 @@ const cols = [...block.firstElementChild.children];
     if (attr != null) {
           const gapName = attr.className.split(" ");
           const gapClass = gapName[1];
-          col.parentElement.classList.add(gapClass);
-         // block.classList.remove(gapClass);
+          col.classList.add(gapClass);
+          block.classList.remove(gapClass);
       }
       col.parentElement.classList.add('grid-container');    
       col.parentElement.setAttribute("style",`grid-template-columns: repeat(${cols.length}, 1fr)`);
