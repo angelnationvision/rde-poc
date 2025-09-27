4f72d81e49f43318bb994639173e5fc14db664d3
angelaccenture <87328367+angelaccenture@users.noreply.github.com>
2025-09-27 09:16:41 -0700

    tryit

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -32,9 +32,9 @@ const gapClass = gapName[1];
            if (col.innerHTML.includes(colgrid)) {
               const matching_elements_cg = all_elements_array.filter(element => element.innerHTML.includes(colgrid));
                 matching_elements_cg.forEach(function(item, index, array) {
-                  
+                   const eleStringcg = item.textContent.trim();
                       console.log(item);
-                    });
+                });
                 const getElementcg = matching_elements_cg.at(-1);
                  const eleStringcg = getElementcg.textContent.trim();
                       col.classList.add(eleStringcg);
